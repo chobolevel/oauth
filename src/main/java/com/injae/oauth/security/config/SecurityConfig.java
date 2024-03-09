23557214c5e37a280b0ebd77ed8e3df255d8a7d7
chobolevel <rodaka123@naver.com>
2024-03-09 14:00:26 +0900

    Refactor: add PasswordEncoderBean

--- src/main/java/com/injae/oauth/security/config/SecurityConfig.java
+++ src/main/java/com/injae/oauth/security/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -25,6 +26,11 @@ public class SecurityConfig {
           formLogin.loginProcessingUrl("/sign/in");
         });
     return http.build();
+  }
+
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 
 }
