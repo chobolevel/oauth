5772f616565db3e5f72eb629d5b6ca9fc1bd6e08
chobolevel <rodaka123@naver.com>
2024-03-10 15:58:12 +0900

    Refactor: 예외 메세지 수정

--- src/main/java/com/injae/oauth/enums/ApiExceptionType.java
+++ src/main/java/com/injae/oauth/enums/ApiExceptionType.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ApiExceptionType {
 
-  MISSING_PARAMETER(HttpStatus.BAD_REQUEST, "[Type: %s]$s이/가 누락되었습니다.");
+  MISSING_PARAMETER(HttpStatus.BAD_REQUEST, "필수 파라미터 (%s)%s 이/가 누락되었습니다.");
 
   private final HttpStatus status;
   private final String message;
