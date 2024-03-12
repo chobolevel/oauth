604806b9a01c302db04def0d5c4bcec3225bd661
chobolevel <rodaka123@naver.com>
2024-03-12 14:14:33 +0900

    Refactor: usernameMatched rules 제거
    remote 활용하여 API 사용하도록 수정

--- src/main/resources/static/js/jquery-custom-validate.js
+++ src/main/resources/static/js/jquery-custom-validate.js
@@ -1,7 +1,3 @@
-jQuery.validator.addMethod('usernameMatched', (value, element) => {
-  console.log(value)
-  console.log(element)
-}, '이미 사용중인 아이디입니다.')
 jQuery.validator.addMethod('korean', (value, element) => {
   const regexp = new RegExp('^[가-힣]+$')
   return regexp.test(value)
