4a5bb16140a0677a591903dc71e281bd24893b75
chobolevel <rodaka123@naver.com>
2024-05-06 00:00:59 +0900

    Refactor: add field

--- src/main/java/com/injae/oauth/entity/User.java
+++ src/main/java/com/injae/oauth/entity/User.java
@@ -1,22 +1,32 @@
 package com.injae.oauth.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLDelete;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Table(name = "users")
 @SQLDelete(sql = "UPDATE users SET is_delete = true WHERE id = ?")
-public class User {
+public class User extends BaseEntity {
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
-
   @Column
   private String username;
-
   @Column
   private String password;
-
+  @Column
+  private String socialId;
+  @Column
+  private String loginType;
   @Column
   private String name;
   @Column
@@ -27,4 +37,5 @@ public class User {
   private String detailAddress;
   @Column
   private boolean isDelete;
+
 }
