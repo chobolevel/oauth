9953f026cbd11aa7421d3e8c0e60e68fa9991451
chobolevel <rodaka123@naver.com>
2024-05-07 00:15:20 +0900

    Feat: add security config

---
+++ src/main/java/com/injae/oauth/security/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.injae.oauth.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http
+        .csrf(csrf -> csrf.disable())
+        .authorizeRequests(authorizeRequests -> {
+          authorizeRequests
+              .requestMatchers("/api/**").permitAll()
+              .anyRequest().permitAll();
+        })
+        .build();
+  }
+
+  @Bean
+  BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+}
