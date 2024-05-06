be79c819d3bc796faec07bb19f3a36677fa44cc1
chobolevel <rodaka123@naver.com>
2024-05-07 00:19:44 +0900

    Refactor: enable jpa auditing

--- src/main/java/com/injae/oauth/OauthApplication.java
+++ src/main/java/com/injae/oauth/OauthApplication.java
@@ -2,8 +2,10 @@ package com.injae.oauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class OauthApplication {
 
   public static void main(String[] args) {
