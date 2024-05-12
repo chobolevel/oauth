c23da5ce741698e42561fb5ee85589d9a16f20d4
chobolevel <rodaka123@naver.com>
2024-05-12 23:58:14 +0900

    Feat: add exception handler

---
+++ src/main/java/com/injae/oauth/advice/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.injae.oauth.advice;
+
+import com.injae.oauth.dto.base.BaseResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<BaseResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(BaseResponse.getInstance(HttpStatus.BAD_REQUEST));
+  }
+
+}
