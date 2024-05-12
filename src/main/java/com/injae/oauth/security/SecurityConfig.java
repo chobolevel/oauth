13fc40439a99d54fb54c5eab0f3b1e96f07869bc
chobolevel <rodaka123@naver.com>
2024-05-12 23:21:01 +0900

    Refactor: disable cors temporary

--- src/main/java/com/injae/oauth/security/SecurityConfig.java
+++ src/main/java/com/injae/oauth/security/SecurityConfig.java
@@ -15,14 +15,9 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    return http
-        .csrf(csrf -> csrf.disable())
-        .authorizeRequests(authorizeRequests -> {
-          authorizeRequests
-              .requestMatchers("/api/**").permitAll()
-              .anyRequest().permitAll();
-        })
-        .build();
+    return http.cors(cors -> cors.disable()).csrf(csrf -> csrf.disable()).authorizeRequests(authorizeRequests -> {
+      authorizeRequests.anyRequest().permitAll();
+    }).build();
   }
 
   @Bean
