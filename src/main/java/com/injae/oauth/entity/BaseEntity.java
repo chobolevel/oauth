a50ac6594b6bb57c1597a1345d821507dd3bfe11
chobolevel <rodaka123@naver.com>
2024-05-12 23:20:29 +0900

    Style: add comments auditing

--- src/main/java/com/injae/oauth/entity/BaseEntity.java
+++ src/main/java/com/injae/oauth/entity/BaseEntity.java
@@ -9,16 +9,16 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
-@MappedSuperclass
-@EntityListeners(value = AuditingEntityListener.class)
+@MappedSuperclass // BaseEntity 상속하는 경우 createdAt, updatedAt 컬럼으로 인식
+@EntityListeners(value = AuditingEntityListener.class) // 해당 클래스에 Auditing 기능을 포함
 public class BaseEntity {
 
   @Column(nullable = false)
-  @CreatedDate
+  @CreatedDate // entity 생성될 때 시간이 자동 저장
   private LocalDateTime createdAt;
 
   @Column(nullable = false)
-  @LastModifiedDate
+  @LastModifiedDate // entity 값을 변경할 때 시간이 자동 저장
   private LocalDateTime updatedAt;
 
 }
